ce6688d6a3b992e08b190bece46a633da22bdbf6
liukiti <liukiti@gmail.com>
2022-02-10 14:29:42 -0300

    fix: just git log

--- semantic-release.sh
+++ semantic-release.sh
@@ -70,7 +70,7 @@ TAGS=$(curl --silent -H  "Authorization: token ${GH_TOKEN}" "${REPOS_URL}/${ORGA
 
 # ---- INIT DATA ----
 echo ${GIT_ARG}
-git log v1.0.0..HEAD
+git log
 SHAS=$(git log ${GIT_ARG} --format="%H %s%b"  | awk -v REGEX_CONVENTIONAL_COMMITS="${REGEX_CONVENTIONAL_COMMITS}" '$2 ~ REGEX_CONVENTIONAL_COMMITS {printf("%s|",$1)}')
 COMMENTS=$(git log ${GIT_ARG} --format="%H %s%b"  | awk -v REGEX_CONVENTIONAL_COMMITS="${REGEX_CONVENTIONAL_COMMITS}" '$2 ~ REGEX_CONVENTIONAL_COMMITS {for(i=2;i<=NF;++i)printf("%s ",$i); printf("|")}')
 
